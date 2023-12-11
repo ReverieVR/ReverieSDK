7f8556375d397e93fb354c4c54e18474a25de23e
Cascadian <90723146+CascadianVR@users.noreply.github.com>
2023-12-11 14:56:06 -0900

    update

--- Scripts/Reverie_CheckSettings.cs
+++ Scripts/Reverie_CheckSettings.cs
@@ -1,22 +1,85 @@
 ﻿#if UNITY_EDITOR
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using UnityEditor;
-using UnityEditor.Callbacks;
 using UnityEngine;
+using System.Text.Json;
 using Debug = UnityEngine.Debug;
 
-namespace ReverieSDK
+namespace ReverieSDK 
 {
     [InitializeOnLoad]
     public static class Reverie_SDK
     {
         static Reverie_SDK()
         {
+            CheckForUpdates();
             CheckProjectSettings();
         }
 
+        [MenuItem("Reverie SDK/Check for Updates")]
+        static void CheckForUpdates()
+        {
+            string currentVersion = File.ReadAllText(Application.dataPath + "/ReverieSDK/version.json");
+            currentVersion = currentVersion.Split(':')[1].Split("\"")[1];
+
+            WebClient wc = new WebClient();
+            string newVersion = wc.DownloadString("https://raw.githubusercontent.com/ReverieVR/ReverieSDK/main/version.json");
+            newVersion = newVersion.Split(':')[1].Split("\"")[1];
+            
+            Debug.Log("Current SDK Version: " + currentVersion);
+            Debug.Log("Latest SDK Version: " + newVersion);
+
+            if (Convert.ToInt16(currentVersion.Replace(".", "")) < Convert.ToInt16(newVersion.Replace(".", "")))
+            {
+                Debug.Log("Updating to Latest SDK Version " + newVersion + "...");
+                
+                DownloadFilesFromGithub();
+            }
+            
+            
+            
+        }
+
+        static async void DownloadFilesFromGithub()
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.UserAgent.Add(
+                new ProductInfoHeaderValue("ReverieSDK", "1"));
+            var repo = "ReverieVR/ReverieSDK";
+            var contentsUrl = $"https://api.github.com/repos/{repo}/contents";
+            var contentsJson = await httpClient.GetStringAsync(contentsUrl);
+            Debug.Log(contentsJson);
+            
+            JsonDocument contents = JsonDocument.Parse(contentsJson);
+            JsonElement root = contents.RootElement;
+
+            foreach (JsonElement element in root.EnumerateArray())
+            {
+                // Access the "type" property
+                string type = element.GetProperty("type").GetString();
+
+                // Check if it's a file or a folder
+                if (type == "file")
+                {
+                    // For files, access the "download_url" property
+                    string downloadUrl = element.GetProperty("download_url").GetString();
+                    Debug.Log(downloadUrl);
+                }
+                else if (type == "dir")
+                {
+                    // For folders, access the "html_url" property
+                    string folderUrl = element.GetProperty("html_url").GetString();
+                    Debug.Log(folderUrl);
+                }
+            }
+        }
+        
         [MenuItem("Reverie SDK/Check Project Settings")]
         static void CheckProjectSettings()
         {
